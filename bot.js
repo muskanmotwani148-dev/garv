21409f9cef20fb690ab8b6deef1d62e0bbbc3ce5
muskanmotwani148-dev <muskanmotwani148@gmail.com>
2026-03-18 16:22:49 +0530

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,41 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const TOKEN = "8463786752:AAGj9eCtkjw9emqI818CjpmVf3ekF-s0bWw";
+const CHANNEL_LINK = "https://t.me/+Fe8o5fbKMntmNzk9";
+
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+
+  const text = `Welcome to GARV TRADING💸 
+! 📊🇮🇳\n\nDaily market insights, charts, and educational trading setups.\nTap the buttons below 👇`;
+
+  const options = {
+    reply_markup: {
+      inline_keyboard: [
+        [{ text: "📢 Join Official Channel", url: CHANNEL_LINK }],
+        [{ text: "I Joined 👍", callback_data: "joined" }]
+      ]
+    }
+  };
+
+  bot.sendMessage(chatId, text, options);
+
+  setTimeout(() => {
+    bot.sendMessage(chatId,
+      "⚠️ Disclaimer:\nWe do NOT provide investment advice.\nAll market analysis is ONLY for educational purposes."
+    );
+  }, 1000);
+});
+
+bot.on('callback_query', (query) => {
+  if (query.data === 'joined') {
+    bot.sendMessage(query.message.chat.id,
+      "✅ Thank you for joining!\n\nYou will now receive daily market insights and educational trading setups.\n\nStay tuned! 📈"
+    );
+    bot.answerCallbackQuery(query.id);
+  }
+});
+
+console.log("Bot is running...");
